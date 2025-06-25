d222cc9e7fbdb35b261cb326b0dae600b44dd221
nwbdashboard <media@newbe.nl>
2025-06-25 16:54:28 +0200

    Update suites.js

--- pages/api/suites.js
+++ pages/api/suites.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     // Initialize Auth (v3.3.0 syntax)
     await doc.useServiceAccountAuth({
       client_email: process.env.GOOGLE_CLIENT_EMAIL,
-      private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      private_key: process.env.GOOGLE_PRIVATE_KEY,
     });
 
     // Load the document properties and worksheets
